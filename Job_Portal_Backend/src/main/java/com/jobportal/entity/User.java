edaa073541e3ca8fd3683823abb5093d2e283b97
Priyanshu Patra <128619186+Priyanshu1238@users.noreply.github.com>
2025-03-29 17:51:06 +0530

    29/3/25 backend

---
+++ Job_Portal_Backend/src/main/java/com/jobportal/entity/User.java
@@ -0,0 +1,38 @@
+package com.jobportal.entity;
+
+
+
+import com.jobportal.dto.AccountType;
+import com.jobportal.dto.UserDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	@Column(unique=true)
+	private String email;
+	private String password;
+	private AccountType accountType;
+	
+	public UserDTO toDTO()
+	{
+		return new UserDTO(this.id,this.name,this.email,this.password,this.accountType);
+	}
+}
